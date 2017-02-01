a1a972de2ed0cf791defe978e965d85d8e73ea5e
yitznewton <yitznewton@hotmail.com>
2017-02-01 13:56:28 -0500

    Add loop controls

---
+++ lib/if_goto.rb
@@ -0,0 +1,40 @@
+class IfGoto
+  def initialize(command)
+    @command = command
+  end
+
+  def to_hack
+    put_value_in_d + conditional_jump
+  end
+
+  private
+
+  attr_reader :command
+
+  def put_value_in_d
+    %Q(
+      @SP
+      AM=M-1
+      D=M
+    )
+  end
+
+  def conditional_jump
+    %Q(
+      @#{scope}.#{label}
+      D;JNE
+    )
+  end
+
+  def scope
+    'LOLdunno'
+  end
+
+  def label
+    words[1]
+  end
+
+  def words
+    command.split(' ')
+  end
+end
